f86d8ee0793da0fb1123c50e13b88b874f9393df
NjohPrince <jufredprince@gmail.com>
2023-03-02 15:05:24 +0100

    feat: override default theme

---
+++ .storybook/camsolTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Camsol.io',
+  brandUrl: 'https://camsol.io',
+  //   brandImage: '/android-chrome-512x512.png',
+  brandTarget: '_self',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import camsolTheme from './camsolTheme';
+
+addons.setConfig({
+  theme: camsolTheme,
+});
