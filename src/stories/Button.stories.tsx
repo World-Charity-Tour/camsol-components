20989d7b9a560e77b31e589ecbd2b5ec88f0109f
NjohPrince <jufredprince@gmail.com>
2023-03-02 16:50:52 +0100

    component: more button variants

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import Button, { ButtonType } from '../components/atoms/button/Button';
+import MailIcon from '../components/icons/MailIcon';
 
 export default {
   title: 'Components/Atoms/Button',
@@ -26,4 +27,61 @@ Outlined.args = {
   onClick: () => {
     alert('You clicked the outlined button');
   },
+};
+
+export const Underlined = Template.bind({});
+Underlined.args = {
+  text: 'Underlined Button',
+  type: ButtonType.UNDERLINED,
+  onClick: () => {
+    alert('You clicked the underlined button');
+  },
+};
+
+export const Text = Template.bind({});
+Text.args = {
+  text: 'Text Button',
+  type: ButtonType.TEXT,
+  onClick: () => {
+    alert('You clicked the text button');
+  },
+};
+
+export const TextWithRightIcon = Template.bind({});
+TextWithRightIcon.args = {
+  text: 'Right Icon Button',
+  type: ButtonType.SOLID,
+  iconRight: (
+    <>
+      <MailIcon fill='#fff' />
+    </>
+  ),
+};
+
+export const TextWithLeftIcon = Template.bind({});
+TextWithLeftIcon.args = {
+  text: 'Left Icon Button',
+  type: ButtonType.SOLID,
+  iconLeft: (
+    <>
+      <MailIcon fill='#fff' />
+    </>
+  ),
+};
+
+export const Customizable = Template.bind({});
+Customizable.args = {
+  text: 'Customizable Button',
+  type: ButtonType.SOLID,
+  iconLeft: (
+    <>
+      <MailIcon fill='#fff' />
+    </>
+  ),
+  bgColor: '#000',
+  hoverBgColor: 'orange',
+  hoverColor: '#fff',
+  color: '#fff',
+  customizeVariantProps: true,
+  index: 0,
 };
